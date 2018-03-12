119489fc67a3a76024f4699b08cd2c5292938df2
JoshD1 <joshdeiner75@gmail.com>
2018-03-12 12:10:25 -0400

    done

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,7 @@
 // Code Product Component Here
 import React from 'react';
 import PropTypes from 'prop-types';
+
 class Product extends React.Component{
 
   render(){
@@ -15,15 +16,21 @@ class Product extends React.Component{
     )
 }
   }
+
   Product.defaultProps = {
-    hasWaterMarks:false,
+    hasWatermark:false,
+
   }
+
+
+
+
 
   Product.propTypes = {
     name: PropTypes.string.isRequired,
     producer: PropTypes.string,
     hasWatermark: PropTypes.bool,
-    color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon'].isRequired),
+    color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
     weight: (props, propName) => {
    const weight = props[propName];
 
@@ -43,4 +50,4 @@ class Product extends React.Component{
  },
 }
 
-export default Product
+export default Product;
